0526cd1fa5d08ae3b1018a94f0b8b86b7971c43f
Aslam Hossain <sahanabaz8@gmail.com>
2026-02-11 20:06:14 +0530

    e-commerce

---
+++ public/myproject.js
@@ -0,0 +1,346 @@
+const get_mobile = document.getElementById("mobile_row");
+const get_essentials = document.getElementById("essentials-row");
+
+
+
+//----------------------------search-input-------------------------//
+function search() {
+  
+
+
+
+
+}
+
+
+
+
+// ------------mobile-mapping------------//
+
+const mobileData = [
+    {
+        image: "/image-mobile/image3(5).png",
+        name: "Galaxy S22 (4GB | 64GB)",
+        price: "29999",
+        delete: "33999",
+        save: "4000",
+
+    },
+
+    {
+        image: "/image-mobile/image3(6).png",
+        name: "Galaxy M13 (4GB | 64GB)",
+        price: "10999",
+        delete: "13999",
+        save: "3000",
+    },
+
+    {
+        image: "/image-mobile/image3(7).png",
+        name: "Galaxy M33 (4GB | 64GB)",
+        price: "16999",
+        delete: "24999",
+        save: "8000",
+    },
+
+    {
+        image: "/image-mobile/image3.png",
+        name: "Galaxy M53 (4GB | 64GB)",
+        price: "31999",
+        delete: "40999",
+        save: "9000",
+    },
+
+    {
+        image: "/image-mobile/image4.png",
+        name: "Galaxy S22 (8GB | 128GB)",
+        price: "67999",
+        delete: "85999",
+        save: "9000",
+    },
+
+    {
+        image: "/image-mobile/image3(5).png",
+        name: "Galaxy S22 (4GB | 64GB)",
+        price: "29999",
+        delete: "33999",
+        save: "4000",
+
+    },
+
+    {
+        image: "/image-mobile/image3(6).png",
+        name: "Galaxy M13 (4GB | 64GB)",
+        price: "10999",
+        delete: "13999",
+        save: "3000",
+    },
+
+    {
+        image: "/image-mobile/image3(7).png",
+        name: "Galaxy M33 (4GB | 64GB)",
+        price: "16999",
+        delete: "24999",
+        save: "8000",
+    },
+
+    {
+        image: "/image-mobile/image3.png",
+        name: "Galaxy M53 (4GB | 64GB)",
+        price: "31999",
+        delete: "40999",
+        save: "9000",
+    },
+
+    {
+        image: "/image-mobile/image4.png",
+        name: "Galaxy S22 (8GB | 128GB)",
+        price: "67999",
+        delete: "85999",
+        save: "9000",
+    },
+
+    {
+        image: "/image-mobile/image3.png",
+        name: "Galaxy M53 (4GB | 64GB)",
+        price: "31999",
+        delete: "40999",
+        save: "9000",
+    },
+
+    {
+        image: "/image-mobile/image4.png",
+        name: "Galaxy S22 (8GB | 128GB)",
+        price: "67999",
+        delete: "85999",
+        save: "9000",
+    }
+
+
+
+
+];
+
+
+// .............................JSON data.................................
+
+const jsondata = JSON.stringify(mobileData);
+const objectdata = JSON.parse(jsondata);
+
+
+const newmobile = objectdata.map((item) => {
+    return `
+    <div class="col-6 col-md-4 col-lg-2">
+                <div id="phone1" class="card product-card text-center p-2">
+
+                    <img src="${item.image}" class="card-img-top" alt="Galaxy S22 Ultra" />
+
+                    <div class="card-body p-2">
+
+                        <p class="card-title small mb-1">"${item.name}"</p>
+                        <p class="price mb-0 fw-bold">${item.price}<span
+                                class="text-decoration-line-through  text-muted small">"${item.delete}"</span></p>
+                        <p class="save text-success small">"${item.save}"</p>
+
+                    </div>
+                </div>
+            </div>
+    `
+}).join("");
+
+
+console.log()
+
+get_mobile.innerHTML = newmobile;
+
+/////...............................................................
+
+/////...............................................................
+
+function aslam() {
+    const select_mobile = document.getElementById("select_option");
+    const get_mobile = document.getElementById("mobile_row");
+    const mobile = select_mobile.value;
+
+
+    let filteredMobiles;
+
+    if (mobile === "All") {
+        filteredMobiles = objectdata
+    } else {
+        filteredMobiles = objectdata.filter((m) => (m.name === mobile))
+    }
+
+    //   const filteredMobiles = mobileData.filter((m) =>( m.name === mobile));
+
+
+    const newFilteredData = filteredMobiles.map((m) => {
+        return `
+      <div class="col-6 col-md-4  col-lg-2">
+        <div class="card product-card text-center p-2 shadow-sm">
+          <img src="${m.image}" class="card-img-top" alt="${m.name}" />
+          <div class="card-body p-2">
+            <p class="card-title small mb-1">"${m.name}"</p>
+            <p class="price mb-0 fw-bold">
+             " ${m.price}"
+              <span class="text-decoration-line-through text-muted small">"${m.delete}"</span>
+            </p>
+            <p class="save text-success small">"${m.save}"</p>
+          </div>
+        </div>
+      </div>
+    `;
+    }).join('');
+
+    get_mobile.innerHTML = newFilteredData;
+}
+
+
+
+
+//............daily essential..............//
+
+const essential = [
+    {
+        image: "/image-fruit/84ef4761d810ce7468c5e5780d068a3fac851f31.png",
+        name: "Daily_essentials",
+        discount: "UP TP 50% OFF",
+
+    },
+
+    {
+        image: "/image-fruit/c20c69140746bd0ca185ecdd3c37b3abed84092e.png",
+        name: "Vegitable",
+        discount: "UP TP 50% OFF",
+    },
+
+    {
+        image: "/image-fruit/f5881a4da43897933a2874adc8f24c5c7a1fd504.png",
+        name: "Fruits",
+        discount: "UP TP 50% OFF",
+    },
+
+    {
+        image: "/image-fruit/013b733683de86d11ef1e91de7b94761d31548f9.png",
+        name: "Strowberry",
+        discount: "UP TP 50% OFF",
+    },
+
+    {
+        image: "/image-fruit/c03fc4d01f2aa15690df58cc030b3f34b1822562.png",
+        name: "Mango",
+        discount: "UP TP 50% OFF",
+    },
+
+    {
+        image: "/image-fruit/d08bba5ab81cd73e763a7950a81e145046ce72c3.png",
+        name: "cherry",
+        discount: "UP TP 50% OFF",
+    },
+
+    {
+        image: "/image-fruit/84ef4761d810ce7468c5e5780d068a3fac851f31.png",
+        name: "Daily_essentials",
+        discount: "UP TP 50% OFF",
+
+    },
+
+    {
+        image: "/image-fruit/c20c69140746bd0ca185ecdd3c37b3abed84092e.png",
+        name: "Vegitable",
+        discount: "UP TP 50% OFF",
+    },
+
+    {
+        image: "/image-fruit/f5881a4da43897933a2874adc8f24c5c7a1fd504.png",
+        name: "Fruits",
+        discount: "UP TP 50% OFF",
+    },
+
+    {
+        image: "/image-fruit/013b733683de86d11ef1e91de7b94761d31548f9.png",
+        name: "Strowberry",
+        discount: "UP TP 50% OFF",
+    },
+
+    {
+        image: "/image-fruit/c03fc4d01f2aa15690df58cc030b3f34b1822562.png",
+        name: "Mango",
+        discount: "UP TP 50% OFF",
+    },
+
+    {
+        image: "/image-fruit/d08bba5ab81cd73e763a7950a81e145046ce72c3.png",
+        name: "cherry",
+        discount: "UP TP 50% OFF",
+    }
+
+
+
+];
+
+
+// .........................JSON data..................................
+
+const jasonessentialdata = JSON.stringify(essential);
+const objectessentialdata = JSON.parse(jasonessentialdata);
+
+
+const newEssential = objectessentialdata.map((e) => {
+    return (
+        `
+        <div class="col-6 col-md-4 col-lg-2">
+
+                <div id="essential1" class="card essential-card text-center p-2">
+
+                    <img src="${e.image}" class="card-img-top" alt="Daily Essentials">
+                    <div class="card-body p-2">
+
+                        <p class="small mb-0">"${e.name}"</p>
+                        <p class="text-muted small">"${e.discount}"</p>
+
+                    </div>
+                </div>
+            </div>
+
+        `
+    )
+}).join("");
+
+get_essentials.innerHTML = newEssential;
+
+// ................................filter data....................................
+
+function hossain() {
+    const get_option = document.getElementById("essentials_option");
+    const get_essentials = document.getElementById("essentials-row");
+    const selectoption = get_option.value;
+
+    let filteredessential;
+
+    if (selectoption === "All") {
+        filteredessential = objectessentialdata;
+    } else {
+        filteredessential = objectessentialdata.filter((f) => f.name === selectoption);
+    }
+
+    const newFilteredessential = filteredessential.map((f) => {
+
+        return `
+      <div class="col-6 col-md-4 col-lg-2">
+        <div class="card essential-card text-center p-2">
+          <img src="${f.image}" class="card-img-top" alt="${f.name}">
+          <div class="card-body p-2">
+            <p class="small mb-0">${f.name}</p>
+            <p class="text-muted small">${f.discount}</p>
+          </div>
+        </div>
+      </div>
+    `
+    }).join("");
+
+    get_essentials.innerHTML = newFilteredessential;
+};
+
+
+
